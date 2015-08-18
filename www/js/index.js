63a82d691245c4dbb5dd7dc5437e973284e597f4
Alvaro Garcia-Moya Herrera <agarcia@webtogo.de>
2015-08-18 16:28:46 +0200

    - Indexjs. Initial version for parsing the XML file and print each contact information.

--- www/js/index.js
+++ www/js/index.js
@@ -22,7 +22,16 @@
 // ####################################################################################################################################################
 
 // Property that represents the map shows in the app
-var file;
+var filePath = "../res/xml/catalog.xml";
+
+// XML TAGS
+var tagCatalog = "catalog";
+var tagGroup = "contact_group";
+var tagPerson = "contact";
+
+var catalog;
+var groups;
+var contacts;
 
 // ###################################################################################################################################################
 // ################################################## PHONEGAP DEFAULT FUNCTIONS #####################################################################
@@ -30,7 +39,7 @@ var file;
 
 // Application Constructor
 function initialize() {
-    console.log("Log: XML parser module starts....");
+    console.log("Log: JAM-app starts..");
     
     // Call to parse the catalog.xml file
     
@@ -59,4 +68,36 @@ function onDeviceReady() {
     receivedElement.setAttribute('style', 'display:block;');
 
     console.log('Received Event: deviceready');
+}
+
+// ###################################################################################################################################################
+// ############################################################ PARSER FUNCTIONS #####################################################################
+// ###################################################################################################################################################
+
+/**
+ * Function that parse the XML file for catalog
+ */
+function parser() {
+    console.log("Log: XML parser initialized..");
+    
+    // Obtain the raw information for the XML file
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", filePath, false);
+    xmlhttp.send();
+    var xmlDoc = xmlhttp.responseXML;     
+    catalog = xmlDoc.getElementsByTagName(tagCatalog);
+    alert("Info: Catalog size is " + catalog.length);
+    
+    // Loop around all groups to get all information
+    groups = [];
+    for (var i = 0; i < catalog.length; i++) {
+        var currentGroup = catalog[i].getElementsByTagName(tagGroup);
+        groups.push(currentGroup);
+        // Now, loop around the current group and print its values TESTING
+        for (int j = 0; j < currentGroup.length; j++) {
+            var currentContact = currentGroup[j].getElementsByTagName(tagPerson);
+            contacts.push(currentContact);
+            console.log("Log: current contact info is " + currentContact);
+        }
+    }
 }
